61f4faef29aac87a13adc878055e62fc6a4ff491
MustafaAyman <mostst124@gmail.com>
2023-11-28 03:28:23 +0200

    updated

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import queryString from "query-string"
 
-const baseURL = "https://moveis-app-lac.vercel.app/api/v1/"
+const baseURL = "https://moon-light-seven.vercel.app/api/v1/"
 
 
 const publicClient = axios.create({
